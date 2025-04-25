88de7956d38f168e14ec28435fe3e0381952fb13
soul-mortal <yodgorsalohiddinov44@gmail.com>
2025-04-25 15:32:30 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const title = document.getElementById('qoyniqoltigi');
+
+
+const color = prompt('Enter a background color:');
+if (color) {
+    title.style.backgroundColor = color;
+}
+
+const textColor = prompt('Enter a text color:');
+if (textColor) {
+    title.style.color = textColor;
+}
+
